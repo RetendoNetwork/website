f1ace60752e21f5ba827422d7eece90cf7f5df2d
00cedke <mathiskera2509@gmail.com>
2024-10-05 13:51:08 +0200

    False layout home page for fix page bug

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', async (req, res) => {
-    res.render('')
+    res.render('home', {
+        layout: false
+    });
 });
 
 module.exports = router;
