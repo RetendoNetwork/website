3ae877aa7a60e9bdde032b0f70986d867c2173fc
00cedke <mathiskera2509@gmail.com>
2024-10-06 16:20:31 +0200

    Updated web pages + javascript server

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ const exphbs = require('express-handlebars');
 const config = require('../config.json');
 const logger = require('./logger');
 const { config: { port } } = config;
+const { config: { url } } = config;
 const path = require('path');
 
 const server = express();
 
-const account = require('./routes/account');
-const home = require('./routes/home');
-const progress = require('./routes/progress');
+const routes = {
+    account: require('./routes/account'),
+    home: require('./routes/home'),
+    progress: require('./routes/progress')
+};
 
 server.use(express.static(path.join(__dirname, '..', 'public')));
 
@@ -17,10 +20,11 @@ server.engine('handlebars', exphbs.engine());
 server.set('view engine', 'handlebars');
 server.set('views', path.join(__dirname, '..', 'views'));
 
-server.use(account);
-server.use(home);
-server.use(progress);
+logger.info(`Applying routes.`);
+server.use(routes.account);
+server.use(routes.home);
+server.use(routes.progress);
 
 server.listen(port, () => {
-    logger.log(`The server was started on http://localhost:${port}`);
+    logger.log(`The server was started on https://${url}:${port}`);
 });
